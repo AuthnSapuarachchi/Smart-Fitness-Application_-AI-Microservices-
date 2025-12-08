7584dce64d1e84156b4f3fc44b277807856aab1c
Authn <authn.sapuarachchi@gmail.com>
2025-12-08 21:29:33 +0530

    implement AI model and get exact recommondation t the user

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,15 +14,17 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAiService aiService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         try {
             log.info("Received activity for processing: {}", activity.getId());
 
-            String recommendation = aiService.generateRecommendation(activity);
+            Recommendation recommendation = aiService.generateRecommendation(activity);
+            recommendationRepository.save(recommendation);
 
-            log.info("Generate Recommendation: {}", recommendation);
+//            log.info("Generate Recommendation: {}", recommendation);
 
         } catch (Exception e) {
 
